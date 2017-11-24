65e954ab8e8cace842c496e6a20d4d1e46e9f4d8
Uditha Kasun <udithakasun311@gmail.com>
2017-11-24 15:36:24 +0530

    Model Population Removed

--- controllers/TerminalController.js
+++ controllers/TerminalController.js
@@ -54,42 +54,14 @@ terminalController.createNewCard = function (details) {
     });
 }
 
-terminalController.checkBalance = function (details) {
-    return cardCreationPromise = new Promise((resolve, reject) => {
-        var passenger = new Passenger(details.passenger);
-        passenger.save().then(function (savedPassenger) {
-            if (details.card.card_type = "REGULAR") {
-                var regularCard = {};
-                regularCard.card_holder = savedPassenger._id;
-                regularCard.card_balance = 0.00;
-                regularCard.card_issued_date = new Date();
-                var regularCardDetails = new RegularCard(regularCard);
-                regularCardDetails.save().then(function () {
-                    resolve({ status : "COMPLETE" , operation : "CARD_CREATED"});
-                }).catch(function(error) {
-                    reject({ status : "PARTIAL" , operation : "PASSENGER_CREATED"});
-                });
-            }
-            else if (details.card.card_type = "TOUR") {
-                var expiryDate = new Date();
-                expiryDate.setDate(result.getDate() + 7);
-
-                var tourCard = {};
-                tourCard.card_holder = savedPassenger._id;
-                tourCard.card_balance = 0.00;
-                tourCard.card_issued_date = new Date();
-                tourCard.card_expiry_date = expiryDate;
-                var tourCardDetails = new TourCard(tourCard);
-                tourCardDetails.save().then(function () {
-                    resolve({ status : "COMPLETE" , operation : "CARD_CREATED"});
-                }).catch(function(error) {
-                    reject({ status : "PARTIAL" , operation : "PASSENGER_CREATED"});
-                });
-            }
-        }).catch(function(error) {
-            reject({ status : "FAILED" , operation : "PASSENGER_CREATION_FAILED"});
+terminalController.checkBalance = function (cardId) {
+    return cardBalancePromise = new Promise((resolve, reject) => {
+        TravelCard.findById(cardId,function(cardDetails){
+            resolve({status : "SUCCESS" , balance : cardDetails.card_balance});
+        })
+        .catch(function(error) {
+            reject({ status : "FAILED" });
         });
-
     });
 }
 
